ce3a3da21589b61a39e5729dae2bb14b1f9d9167
Mahathi V <mahathiv14@outlook.com>
2020-08-26 23:14:25 -0500

    populate basic data on search page

--- App.js
+++ App.js
@@ -1,22 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import 'mockData';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
+import { search, movie } from "./mockData";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  movieTitles = () => {
+    return search["Search"].map((item) => (
+      <View key={item.imdbID}>
+        <Image style={styles.posterView} source={{ uri: `${item.Poster}` }} />
+        <Text style={styles.movieTitleView}>{item["Title"]}</Text>
+      </View>
+    ));
+  };
+
+  return <ScrollView>{this.movieTitles()}</ScrollView>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  posterView: {
+    height: 400,
+    resizeMode: "contain",
+  },
+  movieTitleView: {
+    fontWeight: "bold",
+    fontSize: 22,
   },
 });
