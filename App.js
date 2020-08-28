2b0a71ecf84a002505b8dff3904784cda13b4a75
Deep-Red <kirbycolebrown@gmail.com>
2020-08-27 20:45:19 -0400

    Styling updates for the search results list.

--- App.js
+++ App.js
@@ -3,32 +3,53 @@ import React from "react";
 import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
 import { search, movie } from "./mockData";
 
-export default function App() {
+export default class App extends React.Component {
   movieTitles = () => {
     return search["Search"].map((item) => (
-      <View key={item.imdbID}>
+      <View key={item.imdbID} style={styles.searchResult}>
         <Image style={styles.posterView} source={{ uri: `${item.Poster}` }} />
         <Text style={styles.movieTitleView}>{item["Title"]}</Text>
       </View>
     ));
   };
 
-  return <ScrollView>{this.movieTitles()}</ScrollView>;
+  searchResults = () => {
+    return <View style={styles.searchResultsRow}>{this.movieTitles()}</View>
+  };
+
+  render () {
+    return (
+        <ScrollView style={styles.container}>{this.searchResults()}</ScrollView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  searchResultsRow: {
+    maxWidth: '90%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    alignSelf: 'center',
+  },
+  searchResult: {
+    width: '45%',
+    alignSelf: "center",
+    backgroundColor: "#000000",
+    marginTop: 10,
+    marginBottom: 10,
   },
   posterView: {
-    height: 400,
+    height: 300,
     resizeMode: "contain",
   },
   movieTitleView: {
     fontWeight: "bold",
-    fontSize: 22,
+    textAlign: "center",
+    color: "#99FFFF",
   },
 });
